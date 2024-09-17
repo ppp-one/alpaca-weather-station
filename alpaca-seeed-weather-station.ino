db922e5fd08f6db47720e3be66111e64ac0993d3
Peter Pihlmann Pedersen <peter@ppp.one>
2024-09-17 16:06:59 +0200

    basic user input

--- alpaca-seeed-weather-station.ino
+++ alpaca-seeed-weather-station.ino
@@ -21,54 +21,41 @@ constexpr auto wordlen{ 9.6f };  // OR 10.0f depending on the channel configurat
 constexpr auto preDelayBR{ bitduration * wordlen * 3.5f * 1e6 };
 constexpr auto postDelayBR{ bitduration * wordlen * 3.5f * 1e6 };
 
-// isSafe
-bool isSafe = false;
+// is_safe
+bool is_safe = false;
 
 // safety limits
+int safety_false_time = 10; // Time in seconds to keep is_safe false if not safe
 float AT_low = 0;
 float AT_high = 30;
 float AH_low = 0;
 float AH_high = 80;
 float AP_low = 90000;
-float AP_high = 100000;
+float AP_high = 110000;
 // add more later
 
 // S700 array
-// AT Air temperature C
-// AH Air humidity %RH
-// AP Barometric pressure Pa
-// LX Light intensity Lux
-// DN Minimum wind direction deg
-// Dm Maximum wind direction deg
-// DA Average wind direction deg
-// SN Minimum wind speed m/s (default), km/h, mph, knots
-// SM Maximum wind speed m/s (default), km/h, mph, knots
-// SA Average wind speed m/s (default), km/h, mph, knots
-// RA Accumulated rainfall mm (default), in
-// RD Duration of rainfall s
-// RI Rainfall intensity mm/h (default), in/h
-// Rp Maximum rainfall intensity mm/h (default), in/h
-// HT Heating temperature C
-// TILT Fall detection
-
 std::unordered_map<std::string, int> S700_key = {
-    {"AT", 0},
-    {"AH", 1},
-    {"AP", 2},
-    {"LX", 3},
-    {"DN", 4},
-    {"Dm", 5},
-    {"DA", 6},
-    {"SN", 7},
-    {"SM", 8},
-    {"SA", 9},
-    {"RA", 10},
-    {"RD", 11},
-    {"RI", 12},
-    {"Rp", 13},
-    {"HT", 14},
-    {"TILT", 15}
+    {"AT", 0}, // AT Air temperature C
+    {"AH", 1}, // AH Air humidity %RH
+    {"AP", 2}, // AP Barometric pressure Pa
+    {"LX", 3}, // LX Light intensity Lux
+    {"DN", 4}, // DN Minimum wind direction deg
+    {"Dm", 5}, // Dm Maximum wind direction deg
+    {"DA", 6}, // DA Average wind direction deg
+    {"SN", 7}, // SN Minimum wind speed m/s (default), km/h, mph, knots
+    {"SM", 8}, // SM Maximum wind speed m/s (default), km/h, mph, knots
+    {"SA", 9}, // SA Average wind speed m/s (default), km/h, mph, knots
+    {"RA", 10}, // RA Accumulated rainfall mm (default), in
+    {"RD", 11}, // RD Duration of rainfall s
+    {"RI", 12}, // RI Rainfall intensity mm/h (default), in/h
+    {"Rp", 13}, // Rp Maximum rainfall intensity mm/h (default), in/h
+    {"HT", 14}, // HT Heating temperature C
+    {"TILT", 15} // TILT Fall detection
 };
+
+int32_t ErrorNumber = 0;
+String ErrorMessage = "\"\"";
 
 EthernetServer server(80);
 Application app;
@@ -90,78 +77,62 @@ String split(String data, char separator, int index)
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-void S700_array()
+void read_sensors()
 {
-  const unsigned long timeStale = 5000; // 5000 ms timeout
-  char separator = ';';
-  auto lastData = rtos::Kernel::Clock::now();
-  auto timeSinceSafe = rtos::Kernel::Clock::now();
-  while(1) {
-    if (!isSafe) {
+  auto time_since_not_safe = rtos::Kernel::Clock::now();
+  bool weather_station_data_received = false;
+  bool safety_check_received = false;
+
+  while(true) {
+    // TODO: wrap in try catch
+
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // read weather station data
+    weather_station_data_received = read_weather_station();
+
+    // if no data received, try once more
+    if (!weather_station_data_received) {
+      rtos::ThisThread::sleep_for(1000);
+      weather_station_data_received = read_weather_station();
+    }
+
+    // safety check of weather station data
+    safety_check_received = safety_check();
+
+    // set is_safe
+    is_safe = weather_station_data_received && safety_check_received;
+
+    // if not safe, keep is_safe false for 10 s, but continue to read sensors
+    if (!is_safe) {
+      time_since_not_safe = rtos::Kernel::Clock::now();
+    }
+    if ((rtos::Kernel::Clock::now() - time_since_not_safe > std::chrono::seconds(safety_false_time)) && is_safe) {
+      is_safe = true;
+    } else {
+      is_safe = false;
+    }
+
+    // testing LED
+    if (!is_safe) {
       digitalWrite(LED_D0, HIGH);
     } else {
       digitalWrite(LED_D0, LOW);
     }
-    digitalWrite(LED_BUILTIN, HIGH);
-    String data = readRS485();
-
-
-    if (data == "") {
-      if (rtos::Kernel::Clock::now() - lastData > std::chrono::milliseconds(timeStale)) {
-        Serial.println("No data received within timeout");
-        isSafe = false;
-      }
-      rtos::ThisThread::sleep_for(1000);
-      continue;
-    } else {
-      lastData = rtos::Kernel::Clock::now();
-    }
-
-    int valint = 0;
-    int found = 0;
-    int strIndex[] = {0, -1};
-    int maxIndex = data.length()-1;
-
-    for(int i=0; i<=maxIndex; i++){
-      if(data.charAt(i)==separator || i==maxIndex){
-          found++;
-          strIndex[0] = strIndex[1]+1;
-          strIndex[1] = (i == maxIndex) ? i+1 : i;
-      }
-
-      if (found == 2) {
-        auto vi = data.substring(strIndex[0], strIndex[1]);
-        found=1;
-
-        if (vi.length() > 2 && valint < 15) {
-          Serial.print(valint);
-          Serial.print(": ");
-          Serial.println(vi);
-          vals[valint] = vi.substring(3).toFloat();
-          valint++;
-        }
-      }
-    }
 
     rtos::ThisThread::sleep_for(500);
-
     digitalWrite(LED_BUILTIN, LOW);
-
-    // safety check
-    isSafe = safetyCheck(); // TODO: if false, make the isSafe stay false for a while
-
     rtos::ThisThread::sleep_for(500);
 
   }
 }
 
-String readRS485() {
+bool read_weather_station() {
   const unsigned long timeout = 500; // 500 ms timeout
   const int maxChars = 256; // Maximum characters to read
   char buffer[maxChars + 1]; // +1 for null terminator
   int charCount = 0;
   auto startTime = rtos::Kernel::Clock::now();
-
 
   RS485.noReceive();
   RS485.beginTransmission();
@@ -179,25 +150,50 @@ String readRS485() {
       }
     }
   }
+
   buffer[charCount] = '\0'; // Null-terminate the string
 
   String val = String(buffer);
-  Serial.println(val);
 
   if (charCount == 0) {
     // Serial.println("No data received within timeout");
-    return "";
+    return false;
   }
 
+  // If the response is valid, parse the values
   if (val.startsWith("0XA;AT") && val.endsWith("TILT=0\r\n")) {
-    return val;
+    int valint = 0;
+    int found = 0;
+    int strIndex[] = {0, -1};
+    int maxIndex = val.length()-1;
+    char separator = ';';
+
+    for(int i=0; i<=maxIndex; i++){
+      if(val.charAt(i)==separator || i==maxIndex){
+          found++;
+          strIndex[0] = strIndex[1]+1;
+          strIndex[1] = (i == maxIndex) ? i+1 : i;
+      }
+
+      if (found == 2) {
+        auto vi = val.substring(strIndex[0], strIndex[1]);
+        found=1;
+
+        if (vi.length() > 2 && valint < 15) {
+          vals[valint] = vi.substring(3).toFloat();
+          valint++;
+        }
+      }
+    }
+    return true;
   } else {
-    Serial.println("Invalid response received");
-    return "";
+    Serial.print("Invalid response received: ");
+    Serial.println(val);
+    return false;
   }
 }
 
-bool safetyCheck() {
+bool safety_check() {
   // check if any values are outside of the safety limits
   // temperature
   if (vals[S700_key["AT"]] < AT_low || vals[S700_key["AT"]] > AT_high) {
@@ -224,7 +220,6 @@ bool safetyCheck() {
   return true;
 }
 
-// define a handler function
 void endPoint(Request &req, Response &res) {
 
   res.set("Content-Type", "application/json");
@@ -233,11 +228,8 @@ void endPoint(Request &req, Response &res) {
   uint32_t ClientID = 0;
   uint32_t ClientTransactionID = 0;
 
-  uint32_t ServerID = 0;
-  uint32_t ServerTransactionID = 0;
-
-  int32_t ErrorNumber = 0;
-  String ErrorMessage = "\"\"";
+  uint32_t ServerID = 0; // TODO: what is this?
+  uint32_t ServerTransactionID = 0; // TODO: what is this? increment?
 
   float Value;
 
@@ -254,7 +246,7 @@ void endPoint(Request &req, Response &res) {
   if (url == "connected") {
     Value = 1;
   } else if (url == "issafe") {
-    Value = isSafe;
+    Value = is_safe;
   } else if (url == "temperature") {
     Value = vals[S700_key["AT"]];
   } else if (url == "humidity") {
@@ -300,8 +292,84 @@ void endPoint(Request &req, Response &res) {
   res.print(Value);
   res.print("}");
 
+  // reset error
+  ErrorNumber = 0;
+  ErrorMessage = "\"\"";
+
 }
 
+void index(Request &req, Response &res) {
+
+  res.set("Content-Type", "text/html; charset=utf-8");
+
+  String index_html = R"(
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Configure safety parameters</title>
+  </head>
+  <body>
+      <h1>Configure safety parameters</h1>
+      <form action="/submit" method="post">
+          <label for="maxTemperature">Max temperature:</label>
+          <input type="number" name="maxTemperature" id="maxTemperature" value=")" + String(AT_high) + R"(" required>
+          <br>
+          <label for="minTemperature">Min temperature:</label>
+          <input type="number" name="minTemperature" id="minTemperature" value=")" + String(AT_low) + R"(" required>
+          <br>
+          <label for="maxHumidity">Max humidity:</label>
+          <input type="number" name="maxHumidity" id="maxHumidity" value=")" + String(AH_high) + R"(" required>
+          <br>
+          <label for="minHumidity">Min humidity:</label>
+          <input type="number" name="minHumidity" id="minHumidity" value=")" + String(AH_low) + R"(" required>
+          <br>
+          <label for="maxPressure">Max pressure:</label>
+          <input type="number" name="maxPressure" id="maxPressure" value=")" + String(AP_high) + R"(" required>
+          <br>
+          <label for="minPressure">Min pressure:</label>
+          <input type="number" name="minPressure" id="minPressure" value=")" + String(AP_low) + R"(" required>
+          <br>
+          <input type="submit" value="Submit">
+      </form>
+  </body>
+  </html>
+  )";
+
+  res.print(index_html);
+}
+
+void submit(Request &req, Response &res) {
+
+  char name[32];
+  char value[32];
+
+  while (req.left()) {
+    if (!req.form(name, 32, value, 32)) {
+      return res.sendStatus(400);
+    }
+
+    res.print(name);
+    res.print(":");
+    res.println(value);
+
+    if (strcmp(name, "maxTemperature") == 0) {
+      AT_high = atof(value);
+    } else if (strcmp(name, "minTemperature") == 0) {
+      AT_low = atof(value);
+    } else if (strcmp(name, "maxHumidity") == 0) {
+      AH_high = atof(value);
+    } else if (strcmp(name, "minHumidity") == 0) {
+      AH_low = atof(value);
+    } else if (strcmp(name, "maxPressure") == 0) {
+      AP_high = atof(value);
+    } else if (strcmp(name, "minPressure") == 0) {
+      AP_low = atof(value);
+    }
+  }
+
+}
 
 void setup() {
   
@@ -325,7 +393,8 @@ void setup() {
 
   // mount the handler to the default router
   // app.use(&fillContext);
-  app.get("/", &endPoint);
+  app.get("/", &index);
+  app.post("/submit", &submit);
 
   app.get("/api/v1/safetymonitor/0/connected", &endPoint);
   app.get("/api/v1/safetymonitor/0/issafe", &endPoint);
@@ -367,7 +436,7 @@ void setup() {
   RS485.setDelays(preDelayBR, postDelayBR);
 
   // start SensorReadThread thread
-  SensorReadThread.start(S700_array);
+  SensorReadThread.start(read_sensors);
 }
 
 void loop(){
--- test_alpaca.py
+++ test_alpaca.py
@@ -1,14 +1,16 @@
-from alpaca.observingconditions import *
 from datetime import datetime
 
-WS = ObservingConditions('192.168.0.12', 0)
+from alpaca.observingconditions import *
+
+WS = ObservingConditions("192.168.0.12", 0)
 
 # WS.Connected = True
 counter = 0
-for i in range(1000):
+for i in range(10):
     temperture = WS.Temperature
+    humidity = WS.Humidity
     counter += 1
 
-    print(datetime.now(), temperture, counter)
+    print(datetime.now(), temperture, humidity, counter)
 
     # time.sleep(1)
